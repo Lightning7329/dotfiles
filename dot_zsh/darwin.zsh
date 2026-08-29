b658bb09feb968c136b331eb1b39a5a7132df079
Lightning7329 <111300162+Lightning7329@users.noreply.github.com>
2026-08-29 21:45:16 +0900

    Split zsh config into common and OS-specific parts

---
+++ dot_zsh/darwin.zsh
@@ -0,0 +1,12 @@
+# ls (BSD)
+alias ls='ls -FG'
+alias ll='ls -AlFGo'
+
+# ls color
+export LSCOLORS=cxgxcxdxbxegedabagacad
+
+# node from homebrew
+export PATH="/opt/homebrew/opt/node@22/bin:$PATH"
+# For compilers to find node@22 you may need to set:
+#  export LDFLAGS="-L/opt/homebrew/opt/node@22/lib"
+#  export CPPFLAGS="-I/opt/homebrew/opt/node@22/include"
---
+++ dot_zsh/linux.zsh
@@ -0,0 +1,3 @@
+# ls (GNU)
+alias ls='ls -F --color=auto'
+alias ll='ls -AlFo --color=auto'
